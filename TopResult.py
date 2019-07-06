7109eccf227c059301918ad70028b4ffb0b022ef
Anton Kanugalawattage <antondilon@gmail.com>
2019-07-06 08:05:42 -0400

    Update TopResult.py

--- TopResult.py
+++ TopResult.py
@@ -1,13 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
+#Search URL
+URL = 'https://www.amazon.ca/s?k=a7s'
 
-URL = 'https://www.amazon.ca/s?k=a7'
+#Requesting the page 
+response = requests.get(URL, headers = {'User-Agent' : 'Mozilla/5.0'})
+soup = BeautifulSoup(response.text, "lxml") #Intializing soup
 
+#Title of the product
+title = soup.findAll("a", {"class": "a-link-normal a-text-normal"})[0].text
 
-res = requests.get(URL, headers = {'User-Agent' : 'Mozilla/5.0'})
-soup = BeautifulSoup(res.text, "lxml")
-price1 = soup.select('.a-price-whole')[0].text
-price2= soup.select('.a-price-fraction')[0].text
-price = price1 + price2
-print(price)
+#Product's page
+productAddress = soup.findAll("a", {"class": "a-link-normal a-text-normal"})[0]['href']
+productURL = 'https://www.amazon.ca' + productAddress
+
+##Price of the product
+priceWhole = soup.select('.a-price-whole')[0].text
+priceDecimal = soup.select('.a-price-fraction')[0].text
+price = priceWhole + priceDecimal
+
+##Image of the product
+imageLink = soup.find('img', attrs = {'srcset' : True})["src"]
