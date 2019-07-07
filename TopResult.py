b6711ffe884e5fc8bcddb5074c4d325f598766b8
Anton Kanugalawattage <antondilon@gmail.com>
2019-07-07 10:55:55 -0400

    Update TopResult.py

--- TopResult.py
+++ TopResult.py
@@ -1,14 +1,18 @@
 '''
 Anton Kanugalwattage
 July 4, 2019
+Amazon Price Watch Application
 '''
 
+import webbrowser 
 import time
 import requests
 from bs4 import BeautifulSoup
 
-product = "pencils"
+#Product to be searched
+product = "notebook"
 
+#Agent to request content
 agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
 
 #Search URL
@@ -22,44 +26,54 @@ soup = BeautifulSoup(response.text, "lxml") #Intializing soup
 sponsored, i = True, 0
 
 while sponsored:
-    if soup.findAll("span", {"class": "a-size-base a-color-secondary"})[i].text == "Sponsored":
+    if soup.findAll("span", {"class": "a-size-base a-color-secondary"})[i].text == "Sponsored": #Checking if sponsored
         sponsored=True
         i+=1
     else:
         sponsored=False
-
-
-#Title of the product
-title = soup.findAll("a", {"class": "a-link-normal a-text-normal"})[i].text.strip()
+'''
+#To avoid sponsored content on the header
+if soup.find('span', {'class':'sponsoredBy__label'}).text=="Sponsored by ":
+    i+=3 #If there are sponsored content on the header there is always 3 products
+'''
 
 #Product's page
 productAddress = soup.findAll("a", {"class": "a-link-normal a-text-normal"})[i]['href']
 productURL = 'https://www.amazon.ca' + productAddress
 
-##Price of the product
-priceWhole = soup.select('.a-price-whole')[i].text
-priceDecimal = soup.select('.a-price-fraction')[i].text
-price = priceWhole + priceDecimal
 
-##Image of the product
-imageLink = soup.find('img', attrs = {'srcset' : True})["src"]
+###################################
 
 
-#Requesting the serach page 
-response = requests.get(productURL, headers = {'User-Agent' :agent})
+##Requesting the product page 
+response = requests.get(productURL, headers = {'User-Agent' : agent})
 soup = BeautifulSoup(response.text, "lxml") #Intializing soup
+
+##Price of the product
+productPrice = soup.find('span', {'class':'a-color-price'}).text.strip() #Accesing through product page to avoid discounts and sponosored products
+
+##Title of the product
+productTitle = soup.find(id='productTitle').text.strip()
 
 #Rating of the product
 rating=soup.find('span', {'class':'a-icon-alt'}).text
 
+##Image of the product
+imageURLScraped = soup.find('img', {"class": 'a-dynamic-image'})['data-a-dynamic-image']
+imageURLList = imageURLScraped.split("\"") #Due to multiple sizes of images spliting to select one
+imageURL = imageURLList[1]
+
 print("")
 print(rating)
 print("")
-print(price)
+print(productPrice)
 print("")
-print(title)
+print(productTitle)
 print("")
 print(productURL)
 print("")
-print(imageLink)
+print(imageURL)
 print("")
+
+webbrowser.open(productURL)
+webbrowser.open(imageURL)
