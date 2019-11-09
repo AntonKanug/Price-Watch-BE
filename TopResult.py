a4ee43587b2cfca2ff7d9a0a061bc2f29494d986
Anton Kanugalawattage <antondilon@gmail.com>
2019-11-09 14:07:18 -0500

    once a day checker and product

--- TopResult.py
+++ TopResult.py
@@ -10,82 +10,94 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-#Product to be searched
-product = "corsair mouse"
-
-#Agent to request content
 agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36'
 
-#Search URL
-URL = 'https://www.amazon.ca/s?k=' + product
+def newProduct():
 
-#Requesting the serach page 
-response = requests.get(URL, headers = {'User-Agent' : agent})
-soup = BeautifulSoup(response.text, "lxml") #Intializing soup
+    #Product to be searched
+    product = "mouse"
 
-#Excluding sponsored content from search results
-sponsored, i = True, 0
+    #Search URL
+    URL = 'https://www.amazon.ca/s?k=' + product
 
-while sponsored:
-    if soup.findAll("span", {"class": "a-size-base a-color-secondary"})[i].text == "Sponsored": #Checking if sponsored
-        sponsored=True
-        i+=1
-    else:
-        sponsored=False
-'''
-#To avoid sponsored content on the header
-if soup.find('span', {'class':'sponsoredBy__label'}).text=="Sponsored by ":
-    i+=3 #If there are sponsored content on the header there is always 3 products
-'''
+    #Requesting the serach page 
+    response = requests.get(URL, headers = {'User-Agent' : agent})
+    soup = BeautifulSoup(response.text, "lxml") #Intializing soup
 
-#Product's page
-productAddress = soup.findAll("a", {"class": "a-link-normal a-text-normal"})[i]['href']
-productURL = 'https://www.amazon.ca' + productAddress
+    #Excluding sponsored content from search results
+    sponsored, i = True, 0
+
+    while sponsored:
+        if soup.findAll("span", {"class": "a-size-base a-color-secondary"})[i].text == "Sponsored": #Checking if sponsored
+            sponsored=True
+            i+=1
+        else:
+            sponsored=False
+    '''
+    #To avoid sponsored content on the header
+    if soup.find('span', {'class':'sponsoredBy__label'}).text=="Sponsored by ":
+        i+=3 #If there are sponsored content on the header there is always 3 products
+    '''
+
+    #Product's page
+    productAddress = soup.findAll("a", {"class": "a-link-normal a-text-normal"})[i]['href']
+    productURL = 'https://www.amazon.ca' + productAddress
 
 
-###################################
+    ###################################
 
 
-##Requesting the product page 
-response = requests.get(productURL, headers = {'User-Agent' : agent})
-soup = BeautifulSoup(response.text, "lxml") #Intializing soup
+    ##Requesting the product page 
+    response = requests.get(productURL, headers = {'User-Agent' : agent})
+    soup = BeautifulSoup(response.text, "lxml") #Intializing soup
 
-##Price of the product
-productPrice = soup.find('span', {'class':'a-color-price'}).text.strip() #Accesing through product page to avoid discounts and sponosored products
+    ##Price of the product
+    productPrice = soup.find('span', {'class':'a-color-price'}).text.strip() #Accesing through product page to avoid discounts and sponosored products
 
-##Title of the product
-productTitle = soup.find(id='productTitle').text.strip()
+    ##Title of the product
+    productTitle = soup.find(id='productTitle').text.strip()
 
-#Rating of the product
-rating=soup.find('span', {'class':'a-icon-alt'}).text
+    #Rating of the product
+    rating=soup.find('span', {'class':'a-icon-alt'}).text
 
-##Image of the product
-imageURLScraped = soup.find('img', {"class": 'a-dynamic-image'})['data-a-dynamic-image']
-imageURLList = imageURLScraped.split("\"") #Due to multiple sizes of images spliting to select one
-imageURL = imageURLList[1]
+    ##Image of the product
+    imageURLScraped = soup.find('img', {"class": 'a-dynamic-image'})['data-a-dynamic-image']
+    imageURLList = imageURLScraped.split("\"") #Due to multiple sizes of images spliting to select one
+    imageURL = imageURLList[1]
 
-##JSON 
+    ##JSON 
+    with open('data.json', mode='r', encoding='utf-8') as listContent:
+        content = json.load(listContent)
+        productInList = False
 
-with open('data.json', mode='r', encoding='utf-8') as listContent:
-    content = json.load(listContent)
+    with open('data.json', mode='w', encoding='utf-8') as listContent:
+        for i in content:
+            if i['title'] == productTitle:
+                productInList = True
 
-with open('data.json', mode='w', encoding='utf-8') as listContent:
-    productInList = True
+        if not productInList:
+            entry = {'title': productTitle, 
+                    'price': [float(productPrice[5:])],
+                    'rating': float(rating[0:4]), 
+                    'URL': productURL,
+                    'image': imageURL}
+            content.append(entry)
 
-    for i in content:
-        if i['title'] == productTitle:
+        json.dump(content, listContent, indent=2)
+
+def checkerOnceDay ():
+
+    with open('data.json', mode='r', encoding='utf-8') as listContent:
+        content = json.load(listContent)
+
+    with open('data.json', mode='w', encoding='utf-8') as listContent:
+        for i in content:
+            response = requests.get(i['URL'], headers = {'User-Agent' : agent})
+            soup = BeautifulSoup(response.text, "lxml")  #Intializing soup
+            productPrice = soup.find('span', {'class':'a-color-price'}).text.strip()  #Price of the product
+
             i['price'].append(float(productPrice[5:]))
-            productInList = False
 
-    if productInList:
-        entry = {'title': productTitle, 
-                'price': [float(productPrice[5:])],
-                'rating': float(rating[0:4]), 
-                'URL': productURL,
-                'image': imageURL}
-        content.append(entry)
-    json.dump(content, listContent, indent=2)
+        json.dump(content, listContent, indent=2)
 
-
-# webbrowser.open(productURL)
-# webbrowser.open(imageURL)
+checkerOnceDay()
