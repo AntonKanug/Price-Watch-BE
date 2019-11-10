3f552ec21efa8ebd517e1832980758ff110eb5fb
Anton Kanugalawattage <antondilon@gmail.com>
2019-11-10 08:13:20 -0500

    main.py update

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ Anton Kanugalwattage
 July 4, 2019
 Amazon Price Watch Application
 '''
+
 import webbrowser 
 import time
 import requests
-from bs4 import BeautifulSoup
 import json
+from bs4 import BeautifulSoup
 from priceChecker import priceChecker
 from newProduct import newProduct
 from pricePlot import pricePlot
 
-#newProduct("Mousepad")
+newProduct("beats studio")
 #priceChecker()
 pricePlot('https://www.amazon.ca/VicTsing-Wireless-Receiver-Adjustment-Computer/dp/B01563HW6U/ref=sr_1_3?keywords=mouse&qid=1573326208&sr=8-3')
