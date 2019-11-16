498880abb8009e656aa2c24984524ed9d618a759
Anton Kanugalawattage <antondilon@gmail.com>
2019-11-16 09:34:53 -0500

    comment pricePlot and case not in list

--- pricePlot.py
+++ pricePlot.py
@@ -1,20 +1,37 @@
+'''
+Anton Kanugalwattage
+Nov 13, 2019
+Function for plotting the prices of a product
+Amazon Price Watch Application
+'''
+
 from matplotlib import pyplot as pylab
 import json
 
 def pricePlot (URL):
     with open('data.json', mode='r', encoding='utf-8') as listContent:
         content = json.load(listContent)
+        #Finding the product
+        productFound = False
         for i in content:
             if i['URL'] == URL:
                 prices = i['price']
                 title = i['title']
-    x = []
-    for i in range(len(prices)):
-        x.append(i)
+                productFound = True
 
-    pylab.figure(num=title)
-    pylab.plot(x,prices,'.-')
-    pylab.xlabel('days')
-    pylab.ylabel('Prices')
-    pylab.grid()
-    pylab.show()
+    if productFound:
+        #Creating x axis
+        x = []
+        for i in range(len(prices)):
+            x.append(i)
+
+        #Creating graph
+        pylab.figure(num=title)
+        pylab.plot(x,prices,'.-')
+        pylab.xlabel('days')
+        pylab.ylabel('Prices')
+        pylab.grid()
+        pylab.show()
+
+    else:
+        print("Product is not found in database")
