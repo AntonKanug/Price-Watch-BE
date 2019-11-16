45cb623e88d6dcf6f62c90b095880cd45c351fa5
Anton Kanugalawattage <antondilon@gmail.com>
2019-11-16 07:06:44 -0500

    data.json

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from priceChecker import priceChecker
 from newProduct import newProduct
 from pricePlot import pricePlot
 
-newProduct("anne pro")
-#priceChecker()
-#pricePlot('https://www.amazon.ca/Corsair-Gaming-Backlit-Optical-CH-9300011-NA/dp/B01D63UU52/ref=sr_1_3?keywords=corsair+mouse&qid=1573326183&sr=8-3')
+#newProduct("macbook pro")
+priceChecker()
+#pricePlot('https://www.amazon.ca/Corsair-CH-9304111-NA-Scimitar-Backlit-Optical/dp/B01MU3DZYN/ref=sr_1_3?keywords=corsair+mouse&qid=1573324170&sr=8-3')
+# pricePlot('https://www.amazon.ca/Corsair-Gaming-Backlit-Optical-CH-9300011-NA/dp/B01D63UU52/ref=sr_1_3?keywords=corsair+mouse&qid=1573326183&sr=8-3')
