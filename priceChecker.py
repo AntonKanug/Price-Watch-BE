aa242a7e16a38ee69413acd68dbc15bad6827fbd
Anton Kanugalawattage <antondilon@gmail.com>
2019-12-24 05:03:06 -0500

    updated function call for sendEmail

--- priceChecker.py
+++ priceChecker.py
@@ -31,40 +31,40 @@ def priceChecker ():
     content = list(collection.find())
     print("")
     for product in content:
-        # try:
-        #Getting data for each URL
-        response = requests.get(product['URL'], headers = {'User-Agent' : agent})
-        soup = BeautifulSoup(response.text, "lxml")  #Intializing soup
-
-        # response = urllib2.urlopen(product['URL']).read()
-        # soup = BeautifulSoup(response.decode('utf-8'), "html.parser")  #Intializing soup
-
-        #Price of the product
-        newPrice = soup.find('span', {'class':'a-color-price'}).text.strip() 
         try:
-            newPriceF = float(newPrice[5:].replace(',',''))
+            #Getting data for each URL
+            response = requests.get(product['URL'], headers = {'User-Agent' : agent})
+            soup = BeautifulSoup(response.text, "lxml")  #Intializing soup
+
+            # response = urllib2.urlopen(product['URL']).read()
+            # soup = BeautifulSoup(response.decode('utf-8'), "html.parser")  #Intializing soup
+
+            #Price of the product
+            newPrice = soup.find('span', {'class':'a-color-price'}).text.strip() 
+            try:
+                newPriceF = float(newPrice[5:].replace(',',''))
+            except:
+                continue
+            oldPrice = product['priceList'][-1]['price']
+
+            #Send Email if price lower than 5%
+            if int(abs((newPriceF - oldPrice)*100/newPriceF)) >= 5 :
+                sendEMail(product['_id'], newPriceF, product['priceToCompare'], product['title'], product['URL'], product['image'], product['emailList'])
+                collection.update_one({'_id': product['_id']}, {'$set': {'priceToCompare': newPriceF}})
+
+            #Appending price to list
+            collection.update({'_id': product['_id']}, 
+            {'$push': {
+                'priceList': {
+                    'price': newPriceF, 
+                    'dateTime': str(datetime.datetime.now())
+                }}})
+
+            #Printing confirmation
+            print("✅  %s - Price Updated"% product['title'])
+            print("💵  $%.2f - New Price  \n📅  %s \n"  % (newPriceF,  str(datetime.datetime.now())))
         except:
-            continue
-        oldPrice = product['priceList'][-1]['price']
-
-        #Send Email if price lower than 5%
-        if int(abs((newPriceF - oldPrice)*100/newPriceF)) >= 5 :
-            # sendEMail(product['_id'], newPriceF)
-            collection.update_one({'_id': product['_id']}, {'$set': {'priceToCompare': newPriceF}})
-
-        #Appending price to list
-        collection.update({'_id': product['_id']}, 
-        {'$push': {
-            'priceList': {
-                'price': newPriceF, 
-                'dateTime': str(datetime.datetime.now())
-            }}})
-
-        #Printing confirmation
-        print("✅  %s - Price Updated"% product['title'])
-        print("💵  $%.2f - New Price  \n📅  %s \n"  % (newPriceF,  str(datetime.datetime.now())))
-        # except:
-        #     print("❌  parser not working")
-        #     exit()
+            print("❌  parser not working")
+            exit()
     
     print("✅  MongoDB updated")
