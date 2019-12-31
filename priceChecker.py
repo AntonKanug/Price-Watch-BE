de2d95e818b0803ed24d38dacf7bc5e4e3eb4cd4
Anton Kanugalawattage <antondilon@gmail.com>
2019-12-31 05:41:22 -0500

    priceChecker

--- priceChecker.py
+++ priceChecker.py
@@ -28,50 +28,50 @@ def priceChecker():
     content = list(collection.find())
     print("")
     for product in content:
-        # try:
-        #Getting data for each URL
-        params = {'access_key': '99ea3af699d6d012f9e7df82ac868e3f', 'url': product['URL']}
-        response = requests.get('http://api.scrapestack.com/scrape', params)
-        soup = BeautifulSoup(response.text, "lxml")  #Intializing soup
-
-        # response = urllib2.urlopen(product['URL']).read()
-        # soup = BeautifulSoup(response.decode('utf-8'), "html.parser")  #Intializing soup
-
-        #Price of the product
-        newPrice = soup.find('span', {'class':'a-color-price'}).text.strip()
-        # availability = soup.find(id='availability').text.strip()
-        if newPrice == "Currently unavailable.":
-            collection.update_one({'_id': product['_id']}, {'$set': {'available': False}})
-            continue
-        else:
-            collection.update_one({'_id': product['_id']}, {'$set': {'available': True}})
-
         try:
-            newPriceF = float(newPrice[5:].replace(',',''))
+            #Getting data for each URL
+            params = {'access_key': '99ea3af699d6d012f9e7df82ac868e3f', 'url': product['URL']}
+            response = requests.get('http://api.scrapestack.com/scrape', params)
+            soup = BeautifulSoup(response.text, "lxml")  #Intializing soup
+
+            # response = urllib2.urlopen(product['URL']).read()
+            # soup = BeautifulSoup(response.decode('utf-8'), "html.parser")  #Intializing soup
+
+            #Price of the product
+            newPrice = soup.find('span', {'class':'a-color-price'}).text.strip()
+            # availability = soup.find(id='availability').text.strip()
+            if newPrice == "Currently unavailable.":
+                collection.update_one({'_id': product['_id']}, {'$set': {'available': False}})
+                continue
+            else:
+                collection.update_one({'_id': product['_id']}, {'$set': {'available': True}})
+
+            try:
+                newPriceF = float(newPrice[5:].replace(',',''))
+            except:
+                collection.update_one({'_id': product['_id']}, {'$set': {'available': False}})
+                continue
+
+            oldPrice = product['priceToCompare']
+            #Send Email if price lower than 5%
+            if int(abs((newPriceF - oldPrice)*100/newPriceF)) >= 5 :
+                # sendEMail(product['_id'], newPriceF, product['priceToCompare'], product['title'], product['URL'], product['image'], product['emailList'])
+                collection.update_one({'_id': product['_id']}, {'$set': {'priceToCompare': newPriceF}})
+
+            #Appending price to list
+            collection.update({'_id': product['_id']}, 
+            {'$push': {
+                'priceList': {
+                    'price': newPriceF, 
+                    'dateTime': str(datetime.datetime.now())
+                }}})
+
+            #Printing confirmation
+            print("✅  %s - Price Updated"% product['title'])
+            print("💵  $%.2f - New Price  \n📅  %s \n"  % (newPriceF,  str(datetime.datetime.now())))
         except:
-            collection.update_one({'_id': product['_id']}, {'$set': {'available': False}})
-            continue
-
-        oldPrice = product['priceToCompare']
-        #Send Email if price lower than 5%
-        if int(abs((newPriceF - oldPrice)*100/newPriceF)) >= 5 :
-            # sendEMail(product['_id'], newPriceF, product['priceToCompare'], product['title'], product['URL'], product['image'], product['emailList'])
-            collection.update_one({'_id': product['_id']}, {'$set': {'priceToCompare': newPriceF}})
-
-        #Appending price to list
-        collection.update({'_id': product['_id']}, 
-        {'$push': {
-            'priceList': {
-                'price': newPriceF, 
-                'dateTime': str(datetime.datetime.now())
-            }}})
-
-        #Printing confirmation
-        print("✅  %s - Price Updated"% product['title'])
-        print("💵  $%.2f - New Price  \n📅  %s \n"  % (newPriceF,  str(datetime.datetime.now())))
-        # except:
-            # print("❌  parser not working")
-            # return "Not Updated", 404
+            print("❌  parser not working")
+            return "Not Updated", 404
     
     print("✅  MongoDB updated")
     return "Updated", 201
