94a20866c8dea2df117e17880fb58036d65bd00b
Anton Kanugalawattage <antondilon@gmail.com>
2020-01-23 08:04:21 -0500

    optimized remove product

--- removeProduct.py
+++ removeProduct.py
@@ -12,17 +12,17 @@ def removeProduct(id, email):
     cluster = MongoClient("mongodb+srv://pwUser:gOpJtmdj6JNWAQpy@pricewatch-zurxa.mongodb.net/test?retryWrites=true&w=majority")
     db = cluster['PriceWatch']
     collection = db['PriceWatch-Products']
-    
-    products = list(collection.find())
 
-    for product in products:
-        if id == product['_id']:
-            emailList = []
-            for emailIn in product['emailList']:
-                if emailIn == email:
-                    emailIn = "" 
-                emailList.append(emailIn)
-            collection.update_one({'_id': id}, {'$set': {'emailList': emailList}})
-            print("✅ " + email + " Removed from " + product['title'])
-            break
-    return "Updated", 201
+    product = collection.find_one({"_id": id})
+    emailList = product['emailList']
+    try:
+        emailList.remove(email)
+    except:
+        return "Not Removed, Email not found", 404
+
+    if not len(emailList):
+       emailList = [""] 
+    collection.update_one({'_id': id}, {'$set': {'emailList': emailList}})
+    print("✅ " + email + " Removed from " + product['title'])
+
+    return "Email Removed", 201
